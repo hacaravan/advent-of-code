782864c88ef4d6e0a51fe7855476e457a26ab680
hacaravan <hacavanagh@gmail.com>
2020-12-07 20:58:53 +0000

    Completed day 3 q2

--- Day_3/day_3_q_2.rb
+++ Day_3/day_3_q_2.rb
@@ -1,27 +1,33 @@
-# We want to go through the text, shifting right three and down one each time
+# We want to go through the text, shifting by different amounts each time
 # We are checking for whether there is a '#' or a '.' character
 # If it's a '#', representing a tree, then we add to the count
 # The pattern is supposed to repeat ad infinitum, but this means we can just use modular arithmetic
 
-input_file = './day_3_input'
 
-# Set this as it could be useful to change it in future
-right_step = 3
-
-# Split the initial text file into an array with one line per entry
-lines_split = File.read(input_file).split(/[\r\n]/)
-
-# All lines are same length so set this now, taken from the length of the first line
-line_length = lines_split[0].length
-
-# Start from left-most position, the current_position only gives position along line
-current_position = 0
-
-tree_count = 0
-
-lines_split.each do |line|
-  tree_count += 1 if line.chars[current_position] == '#'
-  current_position = (current_position + right_step) % line_length
+def slope_tree_count(input_file, right_step, down_step)
+  # Split the initial text file into an array with one line per entry
+  lines_split = File.read(input_file).split(/[\r\n]/)
+  # All lines are same length so set this now, taken from the length of the first line
+  line_length = lines_split[0].length
+  line_count = lines_split.length
+  tree_count = 0
+  # Start from top left-most position, the current_position gives across, down position
+  col = 0
+  row = 0
+  while row < line_count do
+    tree_count += 1 if lines_split[row].chars[col] == '#'
+    col = (col + right_step) % line_length
+    row += down_step
+  end
+  tree_count
 end
 
-puts tree_count
+input_file = './day_3_input'
+
+check_1 = slope_tree_count(input_file,1,1)
+check_2 = slope_tree_count(input_file,3,1)
+check_3 = slope_tree_count(input_file,5,1)
+check_4 = slope_tree_count(input_file,7,1)
+check_5 = slope_tree_count(input_file,1,2)
+
+puts check_1 * check_2 * check_3 * check_4 * check_5
