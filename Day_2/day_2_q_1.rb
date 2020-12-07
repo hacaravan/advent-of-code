938319d0e551f1034440671a85197a8327369e41
hacaravan <hacavanagh@gmail.com>
2020-12-07 16:31:02 +0000

    Created day 2 folder and started trying to rephrase the specs of the question

---
+++ Day_2/day_2_q_1.rb
@@ -0,0 +1,12 @@
+# Check the list of passwords combined with their policy, and count how many are valid
+# Valid passwords are ones where the given character appears between the min and max number of times given
+
+input_file = "./day_2_input"
+
+# Split the lines of the input file
+
+# For each line, work out the necessary letter and the min and max number of times it can be used
+
+# Check whether the given password matches the min and max criteria
+
+# Count the number of passwords that are valid and invalid
