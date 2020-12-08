762f71fdb27b60cf179cef83531b84d3fe23ad37
hacaravan <hacavanagh@gmail.com>
2020-12-08 17:40:25 +0000

    Added explanatory notes to q1

--- Day_4/day_4_q_1.rb
+++ Day_4/day_4_q_1.rb
@@ -1,9 +1,7 @@
 # Check how many valid passports there are in the input file
 # Valid passports are those with all seven of the compulsary fields
 
-
-# First, split the input file into different passports, which are separated by
-# two blank lines
+# Define a method to check for valid passports to keep the code clean
 
 def passport_check(passport)
   compulsary_fields = ["byr:", "iyr:", "eyr:", "hgt:", "hcl:", "ecl:", "pid:"]
@@ -13,6 +11,7 @@ def passport_check(passport)
   end
 end
 
+# Set the parameters for this particular question
 
 input_file = './day_4_input'
 passport_array = File.read(input_file).split("\n\n")
@@ -20,9 +19,13 @@ valid_count = 0
 invalid_count = 0
 total_count = 0
 
+# Check every password in the input file
+
 passport_array.each do |passport|
   passport_check(passport) ? valid_count += 1 : invalid_count += 1
   total_count += 1
 end
+
+# Output the result to the screen
 
 puts "#{total_count} passports checked", "#{valid_count} valid", "#{invalid_count} invalid"
