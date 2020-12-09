59f19db2537c1096fac24c9f8ff86f745020073c
hacaravan <hacavanagh@gmail.com>
2020-12-09 21:59:38 +0000

    Completed day 6 q 2

--- Day_6/day_6_q_2.rb
+++ Day_6/day_6_q_2.rb
@@ -1,12 +1,17 @@
 # Take the input, which represent the positive answers of different people (separated by line)
 # in different groups (separated by a blank line) to questions named a-z
-# In this exercise, need to work out how many questions in each group that at
-# least one person in that group responded yes to, then sum the result over all groups
+# In this exercise, need to work out how many questions in each group that
+# everyone in that group responded yes to, then sum the result over all groups
+
+
+def group_consistent_answers(group)
+  group_member_count = group.lines.count
+  unique_chars = group.chars.uniq.select{|char| /[a-z]/.match?(char)}
+  unique_chars.select{|char| group.count(char) == group_member_count}.length
+end
 
 input_file = ('./day_6_input')
 
 answers_by_group = File.read(input_file).split(/\n\n/)
 
-answers_per_group = answers_by_group.map{|answers| answers.chars.uniq.select {|char| /[a-z]/.match?(char)}.length}
-
-puts  answers_per_group.reduce(0, :+)
+puts answers_by_group.map{|group| group_consistent_answers(group)}.reduce(0, :+)
