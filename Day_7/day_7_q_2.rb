24de2ae05e66742a8c9233151a147565756c70ab
hacaravan <hacavanagh@gmail.com>
2020-12-11 13:05:31 +0000

    Completed day 7 question 2 after some time

--- Day_7/day_7_q_2.rb
+++ Day_7/day_7_q_2.rb
@@ -10,36 +10,58 @@ bag_rules_list = File.read(input_file).split("\n")
 # The list is already unique because of how the input file is set out
 bags_list = bag_rules_list.map { |line| line.partition(" bags").first }
 
+# Each key in this hash will be one of the bags, and each value will initially be
+# an empty hash
 bag_container_hash = {}
 
-bags_list.each { |bag| bag_container_hash[bag] = [] }
+bags_list.each { |bag| bag_container_hash[bag] = Hash.new(0) }
 
-# The below adds all the bags that each bag immediately contains, the same number of times as they are contained
-
+# We now cycle through all the bag containment rules, so we can update the hashes
+# within the bag_container_hash. Each hash, which corresponds to a bag,
+# will contain the bags contained within that bag as keys, and the number of times
+# each of these contained bags appears in the container bag as values
 bag_rules_list.each do |line|
   container_bag = line.partition(" bags").first
   contained_bags = line.partition("contain ").last.split(/[.,] *|bags*/).map(&:strip).reject{|bag| bag.empty? || bag == "no other"}
   contained_bags.each do |bag_with_count|
     count = bag_with_count.partition(/\d /).reject(&:empty?).first.to_i
     bag = bag_with_count.partition(/\d /).last
-    count.times { bag_container_hash[container_bag] << bag }
+    bag_container_hash[container_bag][bag] = count
   end
 end
 
-
-# Run through and add all the bags that are contained in each bag that is contained by the given bag,
-# then the bags that those contain, and so on until you get to bags which don't hold any others
-# This is currently incredibly inefficient
+# Set the bag which we're checking to see how many bags are ultimately within it
 check_bag = "shiny gold"
-pass = 0
+
+# initiate an array that (at first) contains the bags directly inside our check_bag as keys,
+# and the number of times each bag appears as values.
+# We will recursively do this same exercise for each bag within descending layers afterwards
+bag_ordered_containment = [bag_container_hash[check_bag]]
+order = 0
+
+# Then for the current order we're looking at, create a new hash for the next layer
+# of bags down, where the keys are the bags in that layer and the values are how
+# many times they are contained in the layer above multiplied by how many times
+# those bags in the layer above appear
+# This process iterates layer by layer until you have a layer which has no bags within it
 while true do
-  initial_array = bag_container_hash[check_bag]
-  new_array = bag_container_hash[check_bag]
-  initial_array.each {|bag| new_array += bag_container_hash[bag]}
-  puts "There are #{new_array.count} contained bags on pass number #{pass}"
-  bag_container_hash[check_bag] = new_array
-  break if bag_container_hash[check_bag] == initial_array
-  pass += 1
+  current_containers = bag_ordered_containment[order]
+  break if current_containers.empty?
+  next_order_bags = Hash.new(0)
+  current_containers.each do |container_bag, container_count|
+    contained_bags = bag_container_hash[container_bag]
+    contained_bags.each do |contained_bag, contained_count|
+      next_order_bags[contained_bag] += container_count * contained_count
+    end
+  end
+  bag_ordered_containment << next_order_bags
+  order += 1
 end
 
-puts "There are #{bag_container_hash[check_bag].count} total bags contained within the #{check_bag} bag"
+total_bags_in_check_bag = bag_ordered_containment.map {|hash| hash.values.reduce(0, :+)}.reduce(0, :+)
+
+puts "There are #{total_bags_in_check_bag} bags within #{check_bag} bag"
+
+
+
+# puts "There are #{bag_container_hash[check_bag].count} total bags contained within the #{check_bag} bag"
