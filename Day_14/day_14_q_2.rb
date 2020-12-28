0f64412bc370848b018e05d75e385612ae2b5dd3
Hugh Cavanagh <hacavanagh@gmail.com>
2020-12-28 12:49:57 +0000

    D14 q2 added notes and successfully completed task

--- Day_14/day_14_q_2.rb
+++ Day_14/day_14_q_2.rb
@@ -5,18 +5,11 @@
 # The floating bit means that both 0 and 1 are used, so e.g. a mask like 0101xx would save to
 # memory locations with 010100, 010101, 010110 and 010111 binary representations
 
-
-# This method uses bitwise and/or ("|" and "&") which compare integers' binary representations
-# where the result of a | b is 0 in each bit which is 0 in both a and b's binary representation
-# and 1 in all other bits. Conversely, a & b gives 1s only when both a and b have 1 in that bit
-# The | operation below converts bits to 1 where they are 1s in the bitmask,
-# and the & puts 0s where there are 0s in the bitmask. Since 'do nothing' is represented
-# by an x in the bitmask, we have to sub this out for the equivalent 'do nothing'
-# bit for each operation - 0 | a = a for all a, 1 & b = b for all b
-
+# Simply iterate through the bits of the bitmask and perform the instructions as given
+# Outputs an array of all the locations that need to be changed given a supposed input
+# memory address, with these converted back to integer from binary string
 def apply_bitmask(original_num)
   binary_original = "%036b" % original_num
-  # p binary_original
   out_arr = [""]
   @mask.chars.each_with_index do |char, index|
     out_arr = add_to_all_in_arr(out_arr, "1") if char == "1"
@@ -26,23 +19,26 @@ def apply_bitmask(original_num)
   out_arr.map{ |location| location.to_i(2)}
 end
 
+# Outputs an array where each element of the array has had the given character appended
 def add_to_all_in_arr(arr, new_char)
   arr.map { |str| str + new_char }
 end
 
+# Outputs an array twice the size of the input where all the strings in the array
+# are present, once with 0 appended and once with 1 appended
 def duplicate_and_add(arr)
   add_to_all_in_arr(arr, "0") + add_to_all_in_arr(arr, "1")
 end
 
 def change_mask(new_mask)
   @mask = new_mask
-  puts "Mask is #{@mask}"
 end
 
 def update_memory_hash(location, value)
   apply_bitmask(location).each { |new_location| @memory_hash[new_location] = value}
 end
 
+# Loop through each line of the instructions and call the relevant method
 def read_instruction(line)
   line_arr = line.split(/[\s\[\]=]/).reject(&:empty?)
   instruction = line_arr.first
@@ -50,22 +46,12 @@ def read_instruction(line)
   update_memory_hash(line_arr[1].to_i, line_arr[2].to_i) if instruction == "mem"
 end
 
-@mask, @memory_hash = "000000000000000000000000000000X1001X", {}
+@mask, @memory_hash = "", {}
 
-# p add_to_all_in_arr(duplicate_and_add(["000000000000000000000000000000"]), "1")
+input_file = "./day_14_input"
 
-p apply_bitmask(42)
+File.read(input_file).each_line { |line| read_instruction(line) }
 
-# "mask = 000000000000000000000000000000X1001X
-# mem[42] = 100
-# mask = 00000000000000000000000000000000X0XX
-# mem[26] = 1".each_line { |line| read_instruction(line) }
+value_sum = @memory_hash.values.reduce(0, :+)
 
-# puts "the output hash is #{@memory_hash}"
-# input_file = "./day_14_input"
-#
-# File.read(input_file).each_line { |line| read_instruction(line) }
-#
-# value_sum = @memory_hash.values.reduce(0, :+)
-#
-# puts "The sum of all stored values is #{value_sum}, or in binary #{value_sum.to_s(2)}"
+puts "The sum of all stored values is #{value_sum}, or in binary #{value_sum.to_s(2)}"
